fde7010cab3313a9eb17a7fa2c0138371e7a951d
Aditya Chaudhary <adviladi.123@gmail.com>
2025-06-21 04:22:05 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,7 +5,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # --- Load Model and Encoders ---
-model = load_model('rnn_model_clean.keras')
+model = load_model("rnn_model_clean.keras", compile=False)
+
 MAX_LEN = 30  # same as training
 
 with open('item_encoder.pkl', 'rb') as f:
