2a32dd61fb5b133d5aa86daa8635ff8aaf8bb4d5
Aditya Chaudhary <adviladi.123@gmail.com>
2025-06-21 05:03:22 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,7 +7,8 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 # --- Load Model and Encoders ---
 from keras.models import load_model
 
-model = load_model("rnn_model_clean.keras", compile=False)
+model = load_model("rnn_model_streamlit.keras", compile=False)
+
 
 MAX_LEN = 30  # same as training
 
